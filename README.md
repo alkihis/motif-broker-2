6aac72ad28d9add3e2276d27c59c5da739d3f454
Louis BERANGER <lberanger@ibcp2117.ibcp.fr>
2019-06-04 14:46:45 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # motif-broker-2
 Front-end service to dispatch and aggregate motif requests on the list of couchDB databases, using [couchdb-dispatcher](https://www.npmjs.com/package/couchdb-dispatcher).
 
-#### Querying the service
+### Running the service
+
+#### Options
+- `-f, --filename <endpointsJsonFile>`
+- `-d, --database <couchDBdatabaseURL>`
+- `-p, --port <couchDBdatabasePort>`
+- `-l, --listen <listenPortOfMotifBroker>`
+
+```bash
+# For example
+node build/index.json -f endpoints.json -d http://localhost -p 5984 -l 3282
+```
+
+### Querying the service
 ```sh
 curl --header "Content-Type: application/json" --request POST --data '{"keys": [ "AAAAAAAAAATTTCAATTATAGG", "AAAAAAAATTATGTTCTTGACGG","AAAAAAACGGACATCCTTTATGG", "AAAACTTCATGCAAGTTTTGCGG","AAAACGGGTTGAATAGTCTCTGG","AAAACGACAATTGCCGTTTTCGG", "AAAAAAAAAAAAAAAATCAATGG", "ATTAAAAAAAAAAGCAGGATGGG"] }' MOTIF-BROKER-2_ENDPOINT/bulk_request
 ```
--- build/index.js
+++ build/index.js
@@ -18,31 +18,44 @@ function readJSON(filename) {
     // Pour chaque couple [regex => endpoint] du JSON 
     for (const [regex, endpoint] of Object.entries(json)) {
         // Au cas où un endpoint a plusieurs regex supportées (j'en doute)
+        const obj_regex = new RegExp(regex);
         if (endpoint in fns) {
             // On sauvegarde la référence de l'ancienne fonction vérificatrice
             const _old = fns[endpoint];
             // La nouvelle fonction = Si la regex actuelle valide OU si l'ancienne fonction valide
-            fns[endpoint] = (key) => (new RegExp(regex)).test(key) || _old(key);
+            fns[endpoint] = (key) => obj_regex.test(key) || _old(key);
         }
         else {
             // On crée la fonction sinon
-            fns[endpoint] = (key) => (new RegExp(regex)).test(key);
+            fns[endpoint] = (key) => obj_regex.test(key);
         }
     }
     return fns;
 }
 program
     .version('0.1.0')
-    .option('-d, --database <databaseUrl>', 'Database URL', "http://localhost")
-    .option('-p, --port <portNum>', 'Port Listening Number', parseInt, 5984)
+    .option('-d, --database <databaseUrl>', 'Database URL (without the port)', "http://localhost")
+    .option('-p, --port <portNum>', 'Database port Number', parseInt, 5984)
+    .option('-l, --listen <portNum>', 'Port Listening Number', parseInt, 3282)
     .option('-f, --filename <fileName>', 'JSON describing RegExp to endpoints', '3letter_prefixe_rules.json')
     .parse(process.argv);
 const DB = `${program.database}:${program.port}`;
 // Initialisation des routes possibles
 const route = new couchdb_dispatcher_1.Routes(readJSON(program.filename), DB);
 route.set('GET', '/handshake', () => undefined, (_, res) => res.json({ handshake: true }));
-// Route bulk standard
-route.set('POST', '/bulk', (req, res) => req.body.keys ? req.body.keys : void res.status(400).json({ error: "Unwell-formed request" }), (_, res, data) => res.json({ request: data }), (_, res, error) => {
+// Route bulk_request standard
+route.set(
+// Route /bulk_request, en méthode POST
+'POST', '/bulk_request', 
+// Récupération des clés: Attendues dans req.body.keys; Sinon, renvoie un bad request
+(req, res) => req.body.keys ? req.body.keys : void res.status(400).json({ error: "Unwell-formed request" }), 
+// Réponses renvoyées par CouchDB renvoyées dans request
+(_, res, data) => res.json({ request: data }), 
+// Si erreur
+(_, res, error) => {
     console.log(error);
     res.status(500).json({ error: "Database error" });
+});
+route.listen(program.listen, () => {
+    console.log(`Listening on port ${program.listen}.`);
 });
