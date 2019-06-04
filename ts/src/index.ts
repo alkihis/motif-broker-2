841718e31deda234e9f4eec8f2891d1393af48a1
Louis BERANGER <lberanger@ibcp2117.ibcp.fr>
2019-06-04 14:37:56 +0200

    Working motif-broker-2

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,4 +1,4 @@
-import program = require('commander');
+import * as program from 'commander';
 import { Routes, EndpointAccepters } from 'couchdb-dispatcher';
 import { readFileSync } from 'fs';
 
@@ -19,16 +19,19 @@ function readJSON(filename: string) {
     // Pour chaque couple [regex => endpoint] du JSON 
     for (const [regex, endpoint] of Object.entries(json)) {
         // Au cas où un endpoint a plusieurs regex supportées (j'en doute)
+
+        const obj_regex = new RegExp(regex);
+
         if (endpoint in fns) {
             // On sauvegarde la référence de l'ancienne fonction vérificatrice
             const _old = fns[endpoint];
 
             // La nouvelle fonction = Si la regex actuelle valide OU si l'ancienne fonction valide
-            fns[endpoint] = (key: string) => (new RegExp(regex)).test(key) || _old(key); 
+            fns[endpoint] = (key: string) => obj_regex.test(key) || _old(key); 
         }
         else {
             // On crée la fonction sinon
-            fns[endpoint] = (key: string) => (new RegExp(regex)).test(key);
+            fns[endpoint] = (key: string) => obj_regex.test(key);
         }
     }
 
@@ -37,8 +40,9 @@ function readJSON(filename: string) {
 
 program
   .version('0.1.0')
-  .option('-d, --database <databaseUrl>', 'Database URL', "http://localhost")
-  .option('-p, --port <portNum>', 'Port Listening Number', parseInt, 5984)
+  .option('-d, --database <databaseUrl>', 'Database URL (without the port)', "http://localhost")
+  .option('-p, --port <portNum>', 'Database port Number', parseInt, 5984)
+  .option('-l, --listen <portNum>', 'Port Listening Number', parseInt, 3282)
   .option('-f, --filename <fileName>', 'JSON describing RegExp to endpoints', '3letter_prefixe_rules.json')
 .parse(process.argv);
 
@@ -51,11 +55,19 @@ route.set('GET', '/handshake', () => undefined, (_, res) => res.json({ handshake
 
 // Route bulk standard
 route.set(
+    // Route /bulk, en méthode POST
     'POST', '/bulk', 
+    // Récupération des clés: Attendues dans req.body.keys; Sinon, renvoie un bad request
     (req, res) => req.body.keys ? req.body.keys : void res.status(400).json({ error: "Unwell-formed request" }), 
+    // Réponses renvoyées par CouchDB renvoyées dans request
     (_, res, data) => res.json({ request: data }), 
+    // Si erreur
     (_, res, error) => { 
         console.log(error); 
         res.status(500).json({ error: "Database error" }); 
     }
 );
+
+route.listen(program.listen, () => {
+    console.log(`Listening on port ${program.listen}.`);
+});
